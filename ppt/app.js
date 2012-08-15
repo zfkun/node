cea19c3700482440b1bc315d4feea19fe7c4f3d7
zfkun <zfkun@msn.com>
2012-08-16 00:01:45 +0800

    fixed & upgrade
    upgrade express version to 3.x;
    upgrade socket.io version to 0.9.10;
    fixed express 2.x code to 3.x;

--- ppt/app.js
+++ ppt/app.js
@@ -1,121 +1,108 @@
+// Inits
+var express = require('express'),
+    app     = express(),
+    server  = app.listen(8080, function() {
+        log.info("PPT server listening on port %d in %s mode", app);
+    }),
+    io      = require('socket.io').listen(server),
+    logger  = require('socket.io/lib/logger'),
+    log     = new logger(),
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-
-var app = module.exports = express.createServer();
-
-var io = require('socket.io').listen(app);
-
-var logger = require('socket.io/lib/logger');
-
-var log = new logger();
+    AUTHOR  = 'zfkun',
+    EMAIL   = EMAIL;
 
 
 // Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.set('view options', { layout : false });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.set('view options', { layout : false });
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler());
 });
 
-// Routes
 
+
+// Routes
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'html5.so - zfkun',
-    author: 'zfkun',
-    email: 'zfkun@msn.com'
-  });
+    res.render('index', { title: 'index', author: AUTHOR, email: EMAIL });
 });
 
 app.get('/html5', function(req, res){
-  res.render('html5', {
-    title: 'html5.so - zfkun',
-    author: 'zfkun',
-    email: 'zfkun@msn.com'
-  });
+    res.render('html5', { title: 'html5', author: AUTHOR, email: EMAIL });
 });
 
 app.get('/ppt', function(req, res){
-  res.render('ppt', {
-    title: 'Node.js - PPT',
-    author: 'zfkun'
-  });
+    res.render('ppt', { title: 'PPT - Viewer', author: AUTHOR });
 });
 
 app.get('/controller', function(req, res){
-  res.render('controller', {
-    title: 'Node.js PPT - Controller',
-    author: 'zfkun'
-  });
+    res.render('controller', { title: 'PPT - Controller', author: AUTHOR });
 });
 
 
 // Sockets
-
 io.sockets.on('connection', function(socket){
-  log.info('new client connected!!');
-  
-  socket.on('set name', function(name, callback){
-    log.info('#' + socket.name, '-> request set name to "', name, '"');
-    socket.name = name;
-    if (typeof callback === 'function') {
-        callback(name);
-    }
-  });
+    log.info('new client connected!!');
+    
+    socket.on('set name', function(name, callback){
+        log.info('#' + socket.name, '-> request set name to "', name, '"');
+        socket.name = name;
+        if (typeof callback === 'function') {
+            callback(name);
+        }
+    });
 
-  socket.on('ppt', function(cmd, args){
-    log.info('#' + socket.name, '-> message(ppt) :', cmd, ',', args);
-    var d = {};
-    if (cmd) {
-        d['command'] = cmd;
-    }
-    if (args && args.length > 0) {
-        d['args'] = args;
-    }
-    socket.broadcast.emit('ppt', d);
-  });
-
-  socket.on('disconnect', function(){
-    log.info(socket.name, ' disconnected!!');
-  });
-  
-  // for test
-  socket.on('message', function(msg){
-    log.debug('#' + socket.name, '-> message(raw) :', msg);
-    var data = JSON.parse(msg);
-    log.debug('#' + socket.name, '-> message(json) :', data);
-    if (data) {
-        log.debug('broadcast(@ppt) before :', data.command, ',', data.args);
+    socket.on('ppt', function(cmd, args){
+        log.info('#' + socket.name, '-> message(ppt) :', cmd, ',', args);
         var d = {};
         if (cmd) {
-            d['command'] = data.command;
+            d['command'] = cmd;
         }
-        if (data.args && data.args.length > 0) {
-            d['args'] = data.args;
+        if (args && args.length > 0) {
+            d['args'] = args;
         }
         socket.broadcast.emit('ppt', d);
-        log.debug('broadcast(@ppt) after :', data.command, ',', data.args);
-    }
-  });
+    });
+
+    socket.on('disconnect', function(){
+        log.info(socket.name, ' disconnected!!');
+    });
+    
+    // for test
+    socket.on('message', function(msg) {
+        var data, d;
+        
+        log.debug('#' + socket.name, '-> message(raw) :', msg);
+        data = JSON.parse(msg);
+        log.debug('#' + socket.name, '-> message(json) :', data);
+        
+        if (data) {
+            log.debug('broadcast(@ppt) before :', data.command, ',', data.args);
+            
+            d = {};
+            if (cmd) d['command'] = data.command;
+            if (data.args && data.args.length > 0) d['args'] = data.args;
+            
+            socket.broadcast.emit('ppt', d);
+            
+            log.debug('broadcast(@ppt) after :', data.command, ',', data.args);
+        }
+    });
   
 });
+
+
 
 // Sockets configure
 io.configure('production', function(){
@@ -129,8 +116,3 @@ io.configure('development', function(){
     io.enable('browser client etag');
     io.set('log level', 1);
 });
-
-
-app.listen(80);
-
-console.log("'Node.js PPT' server listening on port %d in %s mode", app.address().port, app.settings.env);
